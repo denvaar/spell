d4c8373b3b166a2333e152c6445c6146e62607ea
Denver Smith <denverpsmith@gmail.com>
2019-12-05 21:52:29 -0700

    async

--- lib/spell.ex
+++ lib/spell.ex
@@ -9,6 +9,17 @@ defmodule Spell do
     |> insert_many(nil)
   end
 
+  def load_words_async(path) do
+    # read file and format contents
+    {:ok, content} = File.read(path)
+
+    content
+    |> split_content_into_words()
+    |> group_words_by_first_letter()
+    |> spawn_tasks_for_each_group()
+    |> await_and_merge_into_trie()
+  end
+
   def insert_many([], trie), do: trie
 
   def insert_many([word | rest_of_words], trie) do
@@ -18,48 +29,33 @@ defmodule Spell do
     )
   end
 
-  def load_words_async(path) do
+  defp await_and_merge_into_trie(tasks) do
     root = %Trie{letter: nil, children: %{}, terminal: false}
-    # read file and format contents
-    {:ok, content} = File.read(path)
 
-    content
-    |> words_from_content()
-    |> Enum.reduce(%{}, fn <<first_letter::binary-size(1), _rest::binary>> = word, buckets ->
-      Map.put(buckets, first_letter, [word | Map.get(buckets, first_letter, [])])
-    end)
-    |> Map.values()
-    |> Enum.map(fn bucket_of_words ->
-      Task.async(fn -> insert_many(bucket_of_words, nil) end)
-    end)
-    |> Enum.reduce(root, fn task, trie ->
+    Enum.reduce(tasks, root, fn task, trie ->
       sub_trie = Task.await(task)
       %{trie | children: Map.merge(trie.children, sub_trie.children)}
     end)
   end
 
-  def words_from_content(content) do
-    content
-    |> String.trim("\n")
-    |> String.split("\n")
-  end
-
-  def check_string(trie, input) do
-    input
-    |> String.split(" ")
-    |> Enum.reduce([], fn w, possible_misspellings ->
-      word = format_word(w)
-
-      case Spell.Trie.word_exists?(trie, word) do
-        true -> possible_misspellings
-        false -> [word | possible_misspellings]
-      end
+  defp spawn_tasks_for_each_group(groups) do
+    groups
+    |> Enum.map(fn group_of_words ->
+      Task.async(fn -> insert_many(group_of_words, nil) end)
     end)
   end
 
-  defp format_word(word) do
-    word
-    |> String.replace(~r/[\p{P}\p{S}]/, "")
-    |> String.downcase()
+  defp group_words_by_first_letter(words) do
+    words
+    |> Enum.reduce(%{}, fn <<first_letter::binary-size(1), _rest::binary>> = word, groups ->
+      Map.put(groups, first_letter, [word | Map.get(groups, first_letter, [])])
+    end)
+    |> Map.values()
+  end
+
+  def split_content_into_words(content) do
+    content
+    |> String.trim("\n")
+    |> String.split("\n")
   end
 end
